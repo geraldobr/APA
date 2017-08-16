a7981daca51c8ceb284043a499572807c7020ce0
geraldobr <31052294+geraldobr@users.noreply.github.com>
2017-08-16 14:27:42 -0300

    Add files via upload

---
+++ Insertion Sort/insertion.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int insertionSort(int arr[]);
+void show(int arr[]);
+
+const int TAM_ARRAY = 10;
+
+int main(void){
+    
+    srand(0);
+
+    int arr[TAM_ARRAY], l = TAM_ARRAY;
+    for(int i=0;i<TAM_ARRAY;i++){
+        arr[i] = rand()%100;
+    }
+
+    cerr << "Caso Aleatório" << endl;
+    show(arr);
+    cerr << "Número de Passos: " << insertionSort(arr) << endl; // Caso Aleatório
+    show(arr);
+
+    for(int j=0;j<TAM_ARRAY;j++){
+        arr[j] = j;
+    }
+
+    cerr << "Melhor Caso" << endl;
+    show(arr);
+    cerr << "Número de Passos: " << insertionSort(arr) << endl; // Melhor Caso
+    show(arr);
+
+    for(int k=0;k<TAM_ARRAY;k++){
+        arr[k] = l;
+        l--;
+    }
+
+    cerr << "Pior Caso" << endl;
+    show(arr);
+    cerr << "Número de Passos: " << insertionSort(arr) << endl; // Pior Caso
+    show(arr);
+
+    return 0;
+}
+
+int insertionSort(int arr[]){
+    int posAtual=0,j, numPassos=0;
+    for(int i=1; i<TAM_ARRAY; i++){
+        posAtual = arr[i];              // - Posição atual atualizada
+        j = i-1;                        // - selecionamos o J na posição anterior
+        while(j>=0 && arr[j]>posAtual){ // - Iniciamos as comparações
+            arr[j+1] = arr[j];          //   de cada iteração do while
+            j--;                        //   caso seja menor, atualiza a posição
+            numPassos++;                //   e decrementa o J
+        }                               
+        arr[j+1] = posAtual;            // - Atualizamos a posição Atual
+    }
+    return numPassos;
+}
+
+void show(int arr[]){
+    
+    for(int i=0; i<TAM_ARRAY; i++){
+        cerr << arr[i] << " ";
+    }
+    cerr << endl;
+}
---
+++ Selection Sort/selection.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int selection(int arr[]);
+void show(int arr[]);
+void troca(int *p1, int *p2);
+
+const int tam = 10;
+
+int main(void){
+    srand(0);
+
+    int arr[tam], l = tam;
+    for(int i=0;i<tam;i++){
+        arr[i] = rand()%100;
+    }
+
+    cerr << "Caso Aleatório" << endl;
+    show(arr);
+    cerr << "Número de Passos: " << selection(arr) << endl; // Caso Aleatório
+    show(arr);
+
+    for(int j=0;j<tam;j++){
+        arr[j] = j;
+    }
+
+    cerr << "Melhor Caso" << endl;
+    show(arr);
+    cerr << "Número de Passos: " << selection(arr) << endl; // Melhor Caso
+    show(arr);
+
+    for(int k=0;k<tam;k++){
+        arr[k] = l;
+        l--;
+    }
+
+    cerr << "Pior Caso" << endl;
+    show(arr);
+    cerr << "Número de Passos: " << selection(arr) << endl; // Pior Caso
+    show(arr);
+
+    return 0;
+}
+
+int selection(int arr[]){
+    int numPassos = 0, menor;
+
+    for(int i=0;i<tam;i++){
+        menor=i;                    // - Valor atualizado da primeira posição
+        for(int j=i+1; j<tam; j++){ // - Inicio do For a procura do menor valor do subcaminho i+1 : final do vetor
+            numPassos++;
+            if(arr[j] < arr[menor])
+                menor = j;
+        }
+        troca(&arr[menor], &arr[i]); // - Ao encontrar menor valor fazemos a troca das posições
+    }
+    return numPassos;
+}
+
+void show(int arr[]){
+    for(int i=0; i<tam; i++){
+        cerr << arr[i] << " ";
+    }
+    cerr << endl;
+}
+
+/* 
+    Função simples para troca dos valores das posições indicadas
+*/
+void troca(int *p1, int *p2){
+    int aux;
+    aux = *p1;
+    *p1 = *p2;
+    *p2 = aux;
+}
